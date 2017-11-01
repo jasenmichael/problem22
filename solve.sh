e3ccef304915c4d104cdd0269c358dbfebbbe777
jasmichael <jasen@ready-builder.com>
2017-11-01 16:19:55 -0500

    changed set the alphabet to their numeric value into a loop

--- solve.sh
+++ solve.sh
@@ -6,32 +6,12 @@ wget https://projecteuler.net/project/resources/p022_names.txt
 echo "   saved p0022_names.txt"
 echo "---------------------------------------------------"
 echo "2: set the alphabet to their numeric value"
-A=1
-B=2
-C=3
-D=4
-E=5
-F=6
-G=7
-H=8
-I=9
-J=10
-K=11
-L=12
-M=13
-N=14
-O=15
-P=16
-Q=17
-R=18
-S=19
-T=20
-U=21
-V=22
-W=23
-X=24
-Y=25
-Z=26
+l=1
+for letter in {A..Z} ; do
+  let $letter="$l"
+  #echo $l;
+  l=`expr $l + 1`;
+done
 echo "---------------------------------------------------"
 echo "3: next parse and sort names"
 #sort, and replace each comma with a line break and save to sortedNames.txt
@@ -87,6 +67,5 @@ echo " "
 echo "What is the total of all the name scores in the file? "
 
 #use paste to add all lines of a text file.
-paste -sd+ nameScore.txt | bc
-paste -sd+ nameScore.txt | bc > ANSWER.txt
+paste -sd+ nameScore.txt | bc 2>&1 | tee ANSWER.txt
 #rm nameVal.txt && rm sortedNames.txt && rm nameScore.txt
